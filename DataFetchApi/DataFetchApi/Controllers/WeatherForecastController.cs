e17303f05fd86a0ce713a11a48b08ae5d5b00fc5
Megha <52164479+megha8473@users.noreply.github.com>
2023-01-24 16:10:43 +0530

    Adding Api code

---
+++ DataFetchApi/DataFetchApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DataFetchApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+       
+        [HttpGet("GetApiResponse")]
+        public string GetApiResponse(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync(url).Result;
+                response.EnsureSuccessStatusCode();
+                var data = response.Content.ReadAsStringAsync().Result;
+                return JsonConvert.SerializeObject(data);
+            }
+        }
+       
+    }
+}
